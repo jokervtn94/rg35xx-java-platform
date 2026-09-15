cfb4bf51daa1361a5966013bd4562c486be77d21
jokervtn94 <68211781+jokervtn94@users.noreply.github.com>
2026-09-15 22:24:30 +0700

    M1.9D: verify headless setColor fillRect pixels

--- miyoo-m1/m1_9/M19LifecycleLauncher.java
+++ miyoo-m1/m1_9/M19LifecycleLauncher.java
@@ -13,35 +13,40 @@ public final class M19LifecycleLauncher {
 
     public static void main(String[] args) throws Exception {
         mark("M1_9_MAIN_ENTER=YES");
-        mark("M1_9_ARG_COUNT=" + args.length);
-        if (args.length != 1) { mark("M1_9_LIFECYCLE_USAGE=launcher <acceptance-midlet.jar>"); System.exit(2); }
-
+        if (args.length != 1) System.exit(2);
         File midletJar = new File(args[0]);
-        mark("M1_9_MIDLET_PATH=" + midletJar.getPath());
-        if (!midletJar.isFile()) { mark("M1_9_MIDLET_JAR_MISSING=" + midletJar.getPath()); System.exit(3); }
-        mark("M1_9_MIDLET_FILE=PASS");
+        if (!midletJar.isFile()) System.exit(3);
 
         System.setProperty("rg35xx.headless.font", "true");
-        mark("M1_9_HEADLESS_FONT=ENABLED");
         System.setProperty("rg35xx.headless.image.probe", "true");
-        mark("M1_9_HEADLESS_IMAGE_PROBE=ENABLED");
         System.setProperty("rg35xx.headless.graphics.probe", "true");
-        mark("M1_9_HEADLESS_GRAPHICS_PROBE=ENABLED");
+        mark("M1_9D_HEADLESS_RENDER=ENABLED");
 
-        mark("M1_9_FONT_SIZE_BEGIN=YES");
         PlatformFont.setScreenSize(640, 480);
         mark("M1_9_FONT_SIZE_END=YES");
-
-        mark("M1_9C_IMAGE_BEGIN=YES");
         PlatformImage image = new PlatformImage(640, 480);
-        mark("M1_9C_IMAGE_END=YES");
-        mark("M1_9C_BUFFER_LENGTH=" + image.getDataBuffer().length);
-
-        mark("M1_9C_GRAPHICS_BEGIN=YES");
         javax.microedition.lcdui.Graphics graphics = image.getMIDPGraphics();
-        mark("M1_9C_GRAPHICS_END=YES");
-        mark("M1_9C_GRAPHICS_BUFFER_LENGTH=" + graphics.getFrameBuffer().length);
-        mark("M1_9C_GRAPHICS_ACCEPTANCE_MARKER=PASS");
-        mark("M1_9C_STOP_BEFORE_RENDER_METHODS=YES");
+        int[] fb = graphics.getFrameBuffer();
+        mark("M1_9D_BUFFER_LENGTH=" + fb.length);
+
+        // Primary variable: MIDP setColor + fillRect only.
+        graphics.setColor(0x00FF00);
+        graphics.fillRect(10, 20, 30, 40);
+
+        int inside = fb[(20 * 640) + 10];
+        int inside2 = fb[(59 * 640) + 39];
+        int outside = fb[(19 * 640) + 10];
+        int outside2 = fb[(60 * 640) + 40];
+        mark("M1_9D_PIXEL_INSIDE_1=" + Integer.toHexString(inside));
+        mark("M1_9D_PIXEL_INSIDE_2=" + Integer.toHexString(inside2));
+        mark("M1_9D_PIXEL_OUTSIDE_1=" + Integer.toHexString(outside));
+        mark("M1_9D_PIXEL_OUTSIDE_2=" + Integer.toHexString(outside2));
+
+        boolean pass = inside == 0xFF00FF00 && inside2 == 0xFF00FF00 &&
+                       outside == 0xFFFFFFFF && outside2 == 0xFFFFFFFF;
+        mark("M1_9D_SETCOLOR_FILLRECT=" + (pass ? "PASS" : "FAIL"));
+        if (!pass) System.exit(20);
+        mark("M1_9D_RENDER_ACCEPTANCE_MARKER=PASS");
+        mark("M1_9D_STOP_BEFORE_SDL_PRESENTER=YES");
     }
 }
