470b3d0ca15b2c7651fc792926389d4915713602
jokervtn94 <68211781+jokervtn94@users.noreply.github.com>
2026-09-16 02:10:56 +0700

    M1.9F-r3: add fail-closed MobilePlatform resize diagnostics patch

---
+++ miyoo-m1/m1_9/patch_mobileplatform_resize_diagnostics.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+p = Path('upstream/src/org/recompile/mobile/MobilePlatform.java')
+s = p.read_text(encoding='utf-8')
+
+def replace_once(old, new, label):
+    global s
+    if s.count(old) != 1:
+        raise SystemExit('FAIL_CLOSED_%s_COUNT=%d' % (label, s.count(old)))
+    s = s.replace(old, new, 1)
+
+replace_once(
+'''\t\tresizeLCD(width, height);''',
+'''\t\tSystem.out.println("M1_9F_MP_RESIZELCD_CALL_BEGIN=YES"); System.out.flush();
+\t\tresizeLCD(width, height);
+\t\tSystem.out.println("M1_9F_MP_RESIZELCD_CALL_END=PASS"); System.out.flush();''',
+'CONSTRUCTOR_RESIZE_CALL')
+
+replace_once(
+'''\t\tlcdWidth = width;
+\t\tlcdHeight = height;
+
+\t\torg.recompile.mobile.PlatformFont.setScreenSize(width, height);
+
+\t\tlcdFrontbuffer = new PlatformImage(width, height);
+\t\tlcd = new PlatformImage(width, height);
+
+
+        gcFrontbuffer = lcdFrontbuffer.getMIDPGraphics();''',
+'''\t\tSystem.out.println("M1_9F_MP_RESIZE_DIMENSIONS_BEGIN=YES"); System.out.flush();
+\t\tlcdWidth = width;
+\t\tlcdHeight = height;
+\t\tSystem.out.println("M1_9F_MP_RESIZE_DIMENSIONS_END=PASS"); System.out.flush();
+
+\t\tSystem.out.println("M1_9F_MP_RESIZE_FONT_BEGIN=YES"); System.out.flush();
+\t\torg.recompile.mobile.PlatformFont.setScreenSize(width, height);
+\t\tSystem.out.println("M1_9F_MP_RESIZE_FONT_END=PASS"); System.out.flush();
+
+\t\tSystem.out.println("M1_9F_MP_FRONTBUFFER_BEGIN=YES"); System.out.flush();
+\t\tlcdFrontbuffer = new PlatformImage(width, height);
+\t\tSystem.out.println("M1_9F_MP_FRONTBUFFER_END=PASS"); System.out.flush();
+\t\tSystem.out.println("M1_9F_MP_BACKBUFFER_BEGIN=YES"); System.out.flush();
+\t\tlcd = new PlatformImage(width, height);
+\t\tSystem.out.println("M1_9F_MP_BACKBUFFER_END=PASS"); System.out.flush();
+
+
+        System.out.println("M1_9F_MP_FRONTGRAPHICS_BEGIN=YES"); System.out.flush();
+        gcFrontbuffer = lcdFrontbuffer.getMIDPGraphics();
+        System.out.println("M1_9F_MP_FRONTGRAPHICS_END=PASS"); System.out.flush();''',
+'RESIZE_ALLOCATION_BLOCK')
+
+replace_once(
+'''\t\tif (!Mobile.isDoJa)
+\t\t{
+\t\t\tgc = lcd.getMIDPGraphics();
+\t\t\tcom.xce.lcdui.XDisplay.width = width;
+\t\t\tcom.xce.lcdui.XDisplay.height2 = height;
+\t\t\tcom.xce.lcdui.XDisplay.platformImage = lcd;
+\t\t\tcom.xce.lcdui.Toolkit.graphics = (Graphics) gc;''',
+'''\t\tif (!Mobile.isDoJa)
+\t\t{
+\t\t\tSystem.out.println("M1_9F_MP_BACKGRAPHICS_BEGIN=YES"); System.out.flush();
+\t\t\tgc = lcd.getMIDPGraphics();
+\t\t\tSystem.out.println("M1_9F_MP_BACKGRAPHICS_END=PASS"); System.out.flush();
+\t\t\tSystem.out.println("M1_9F_MP_XCE_BRIDGE_BEGIN=YES"); System.out.flush();
+\t\t\tcom.xce.lcdui.XDisplay.width = width;
+\t\t\tcom.xce.lcdui.XDisplay.height2 = height;
+\t\t\tcom.xce.lcdui.XDisplay.platformImage = lcd;
+\t\t\tcom.xce.lcdui.Toolkit.graphics = (Graphics) gc;
+\t\t\tSystem.out.println("M1_9F_MP_XCE_BRIDGE_END=PASS"); System.out.flush();''',
+'RESIZE_XCE_BLOCK')
+
+p.write_text(s, encoding='utf-8')
+print('M1_9F_R3_MOBILEPLATFORM_RESIZE_DIAGNOSTICS_PATCH=PASS')
