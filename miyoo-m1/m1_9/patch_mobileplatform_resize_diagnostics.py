32bb3c6fc5bf37c9a9983ef5cc1c3964fade0c2d
jokervtn94 <68211781+jokervtn94@users.noreply.github.com>
2026-09-16 02:17:20 +0700

    M1.9F-r4: isolate constructor encoding boundary before resizeLCD

--- miyoo-m1/m1_9/patch_mobileplatform_resize_diagnostics.py
+++ miyoo-m1/m1_9/patch_mobileplatform_resize_diagnostics.py
@@ -11,11 +11,48 @@ def replace_once(old, new, label):
     s = s.replace(old, new, 1)
 
 replace_once(
-'''\t\tresizeLCD(width, height);''',
-'''\t\tSystem.out.println("M1_9F_MP_RESIZELCD_CALL_BEGIN=YES"); System.out.flush();
+'''\t\tboolean isUsingValidEncoding = false;
+
+\t\t// Check whether we're using any of the valid encodings before starting the jar, otherwise we'll be defaulting to ISO_8859_1
+\t\tfor (String encoding : Mobile.supportedEncodings)
+\t\t{
+\t\t\tif (encoding.equals(System.getProperty("file.encoding"))) { isUsingValidEncoding = true; }
+\t\t}
+
+\t\tif(!isUsingValidEncoding)
+\t\t{
+\t\t\tMobile.textEncoding = Mobile.supportedEncodings[Mobile.ISO_8859_1];
+\t\t\tcheckFileEncoding();
+\t\t}
+
+\t\tresizeLCD(width, height);''',
+'''\t\tSystem.out.println("M1_9F_MP_ENCODING_INIT_BEGIN=YES"); System.out.flush();
+\t\tboolean isUsingValidEncoding = false;
+\t\tSystem.out.println("M1_9F_MP_ENCODING_PROPERTY=" + System.getProperty("file.encoding")); System.out.flush();
+
+\t\t// Check whether we're using any of the valid encodings before starting the jar, otherwise we'll be defaulting to ISO_8859_1
+\t\tSystem.out.println("M1_9F_MP_ENCODING_SCAN_BEGIN=YES"); System.out.flush();
+\t\tfor (String encoding : Mobile.supportedEncodings)
+\t\t{
+\t\t\tif (encoding.equals(System.getProperty("file.encoding"))) { isUsingValidEncoding = true; }
+\t\t}
+\t\tSystem.out.println("M1_9F_MP_ENCODING_SCAN_END=PASS VALID=" + isUsingValidEncoding); System.out.flush();
+
+\t\tif(!isUsingValidEncoding)
+\t\t{
+\t\t\tSystem.out.println("M1_9F_MP_ENCODING_FALLBACK_BEGIN=YES"); System.out.flush();
+\t\t\tMobile.textEncoding = Mobile.supportedEncodings[Mobile.ISO_8859_1];
+\t\t\tSystem.out.println("M1_9F_MP_CHECK_FILE_ENCODING_BEGIN=YES"); System.out.flush();
+\t\t\tcheckFileEncoding();
+\t\t\tSystem.out.println("M1_9F_MP_CHECK_FILE_ENCODING_END=PASS"); System.out.flush();
+\t\t\tSystem.out.println("M1_9F_MP_ENCODING_FALLBACK_END=PASS"); System.out.flush();
+\t\t}
+\t\tSystem.out.println("M1_9F_MP_ENCODING_INIT_END=PASS"); System.out.flush();
+
+\t\tSystem.out.println("M1_9F_MP_RESIZELCD_CALL_BEGIN=YES"); System.out.flush();
 \t\tresizeLCD(width, height);
 \t\tSystem.out.println("M1_9F_MP_RESIZELCD_CALL_END=PASS"); System.out.flush();''',
-'CONSTRUCTOR_RESIZE_CALL')
+'CONSTRUCTOR_ENCODING_AND_RESIZE')
 
 replace_once(
 '''\t\tlcdWidth = width;
@@ -72,4 +109,4 @@ replace_once(
 'RESIZE_XCE_BLOCK')
 
 p.write_text(s, encoding='utf-8')
-print('M1_9F_R3_MOBILEPLATFORM_RESIZE_DIAGNOSTICS_PATCH=PASS')
+print('M1_9F_R4_MOBILEPLATFORM_CONSTRUCTOR_DIAGNOSTICS_PATCH=PASS')
