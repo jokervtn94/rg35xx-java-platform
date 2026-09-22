cc957a434e41d03fbd27b03b8473067903a77357
jokervtn94 <68211781+jokervtn94@users.noreply.github.com>
2026-09-22 18:37:22 +0700

    DP-R5: keep collision diagnostic scoped to width-height order

--- rebuild-v5/DP5SpritePixelCollisionDiagnosticMIDlet.java
+++ rebuild-v5/DP5SpritePixelCollisionDiagnosticMIDlet.java
@@ -21,7 +21,6 @@ public final class DP5SpritePixelCollisionDiagnosticMIDlet extends MIDlet implem
     }
 
     public void run() {
-        boolean all = true;
         try {
             final int T = 0x00000000;
             final int R = 0xFFFF0000;
@@ -42,7 +41,6 @@ public final class DP5SpritePixelCollisionDiagnosticMIDlet extends MIDlet implem
             Sprite b = new Sprite(bImg);
 
             a.setPosition(0, 0);
-            b.setPosition(0, 0);
             require("DP_R5_SAMEPOS_BOUNDING_TRUE", a.collidesWith(b, false));
             require("DP_R5_SAMEPOS_PIXEL_TRUE", a.collidesWith(b, true));
 
@@ -54,8 +52,7 @@ public final class DP5SpritePixelCollisionDiagnosticMIDlet extends MIDlet implem
             require("DP_R5_FAR_BOUNDING_FALSE", !a.collidesWith(b, false));
             require("DP_R5_FAR_PIXEL_FALSE", !a.collidesWith(b, true));
 
-            // Explicit 3x2 rectangular images. Offset x=1 creates a 2x2 overlap.
-            // Offset y=1 creates a 3x1 overlap. Both must execute without exception.
+            // Explicit 3x2 rectangular images.
             Image cImg = Image.createRGBImage(new int[] {
                 R, T, B,
                 T, T, T
@@ -68,45 +65,21 @@ public final class DP5SpritePixelCollisionDiagnosticMIDlet extends MIDlet implem
             Sprite c = new Sprite(cImg);
             Sprite d = new Sprite(dImg);
 
+            // x-offset => 2x2 overlap. Opaque pixels do not coincide.
             c.setPosition(0, 0);
             d.setPosition(1, 0);
-            boolean rectX = c.collidesWith(d, true);
-            System.out.println("DP_R5_RECT_X_PIXEL_RETURN=PASS actual=" + rectX);
+            require("DP_R5_RECT_X_PIXEL_FALSE", !c.collidesWith(d, true));
 
+            // y-offset => 3x1 overlap. Lower source row is transparent.
             d.setPosition(0, 1);
-            boolean rectY = c.collidesWith(d, true);
-            System.out.println("DP_R5_RECT_Y_PIXEL_RETURN=PASS actual=" + rectY);
+            require("DP_R5_RECT_Y_PIXEL_FALSE", !c.collidesWith(d, true));
 
-            // Transform smoke on pixel collision: no exception, expected self overlap true.
-            int[] transforms = new int[] {
-                Sprite.TRANS_NONE,
-                Sprite.TRANS_ROT90,
-                Sprite.TRANS_ROT180,
-                Sprite.TRANS_ROT270,
-                Sprite.TRANS_MIRROR,
-                Sprite.TRANS_MIRROR_ROT90,
-                Sprite.TRANS_MIRROR_ROT180,
-                Sprite.TRANS_MIRROR_ROT270
-            };
-
-            for (int i = 0; i < transforms.length; i++) {
-                Sprite x = new Sprite(cImg);
-                Sprite y = new Sprite(cImg);
-                x.setTransform(transforms[i]);
-                y.setTransform(transforms[i]);
-                x.setPosition(0, 0);
-                y.setPosition(0, 0);
-                boolean hit = x.collidesWith(y, true);
-                require("DP_R5_TRANSFORM_" + i + "_SELF_PIXEL_TRUE", hit);
-            }
-
-            System.out.println("DP_R5_TRANSFORM_COUNT=8");
+            System.out.println("DP_R5_NON_SQUARE_OVERLAPS=2");
             System.out.println("DP_R5_DIAGNOSTIC_GATE=PASS");
             System.out.println("DP_R5_RUNTIME_GATE=PASS");
             notifyDestroyed();
             System.exit(0);
         } catch (Throwable t) {
-            all = false;
             System.out.println("DP_R5_DIAGNOSTIC_GATE=FAIL");
             System.out.println("DP_R5_RUNTIME_GATE=FAIL");
             System.out.println("DP_R5_FATAL_EXCEPTION=" + t.getClass().getName() + ":" + t.getMessage());
