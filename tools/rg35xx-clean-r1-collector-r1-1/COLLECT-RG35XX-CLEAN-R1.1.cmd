c84373ac60b3a46325f4d90739afce7b5908bed3
jokervtn94 <68211781+jokervtn94@users.noreply.github.com>
2026-09-22 10:39:29 +0700

    tools: add COLLECT-RG35XX-CLEAN-R1.1.cmd

---
+++ tools/rg35xx-clean-r1-collector-r1-1/COLLECT-RG35XX-CLEAN-R1.1.cmd
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0COLLECT-RG35XX-CLEAN-R1.1.ps1" %*
+if errorlevel 1 (
+ echo COLLECT FAILED
+ pause
+ exit /b 1
+)
+pause
